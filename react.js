953d5fcb73ace535c6180bdf5bf190d8ad42f8f2
student <koreansungkyu@gmail.com>
2018-08-06 09:16:27 -0600

    oof

---
+++ react.js
@@ -0,0 +1,10 @@
+import React, { Component} from 'react';
+export default class React extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My react starter</h1>
+            </div>
+        )
+    }
+}
